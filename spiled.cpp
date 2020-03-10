c88c7f990039af5201c731bd6c6e9d12bc4d4019
Rob <rblans8@gmail.com>
2020-03-10 00:08:06 -0500

    Add debug output..

--- spiled.cpp
+++ spiled.cpp
@@ -44,18 +44,19 @@ static uint8_t mode;
 static uint8_t bits = 8;
 static uint32_t speed = 8000000;
 static uint16_t delay;
-
-// Space for 16x16 24-bit (8-bits per color) LEDs
-// My set up uses each SPI byte to encode 2 bits of the LED data.
-// Append REFRESH (zeros) to cause the freshly written data to be latched.
-// Datasheet says it should be at least 280 us / at 8Mbs, that's 1 us per byte.
-// Hence 280 additional bytes.
+static uint16_t pattern = 0;
 
 static const uint16_t GRID_WIDTH = 16;
 static const uint16_t GRID_HEIGHT = 16;
 static const uint16_t REFRESH_SIZE = 280;
 static const uint16_t BITS_PER_SPI_BYTE = 2;
 static const uint16_t SPI_BYTES_PER_BYTE = 4;
+
+// Space for 16x16 24-bit (8-bits per color) LEDs
+// My set up uses each SPI byte to encode 2 bits of the LED data.
+// Append REFRESH (zeros) to cause the freshly written data to be latched.
+// Datasheet says it should be at least 280 us / at 8Mbs, that's 1 us per byte.
+// Hence 280 additional bytes.
 
 static const uint16_t txBuffer_SIZE = GRID_WIDTH * GRID_HEIGHT * 3 + 280;
 static uint8_t txBuffer[txBuffer_SIZE] = {0, }; 
@@ -96,8 +97,8 @@ static void spiGridClear()
 }
 
 // NOTE: You have to pass in the spiPixel for this to fill and return.
-static struct spiRgbPixel_t& 
-    makeSpiPixel(struct spiRgbPixel_t& spiPixel, struct rgbPixel_t& rgb)
+static spiRgbPixel_t& 
+    makeSpiPixel(spiRgbPixel_t& spiPixel, const rgbPixel_t& rgb)
 {
     uint8_t mapBits [4] = {
         _0_0,	// 10001000 - represents 00
@@ -106,24 +107,28 @@ static struct spiRgbPixel_t&
         _1_1, 	// 11001100 - represents 11
     };
 
-    for (uint16_t bytePos = SPI_BYTES_PER_BYTE-1; bytePos >= 0; bytePos--)
+    // Get copies because shifting zeros the byte.
+    uint8_t r = rgb.r;
+    uint8_t g = rgb.g;
+    uint8_t b = rgb.b;
+    for (int8_t bytePos = SPI_BYTES_PER_BYTE-1; bytePos >= 0; bytePos--)
     {
         // Needs 1 SPI byte per 2 bits of LED color data.
-        spiPixel.r[bytePos] = mapBits[rgb.r & 0x03];
-        rgb.r >>= 2;
+        spiPixel.r[bytePos] = mapBits[r & 0x03];
+        r >>= 2;
 
-        spiPixel.g[bytePos] = mapBits[rgb.g & 0x03];
-        rgb.g >>= 2;
+        spiPixel.g[bytePos] = mapBits[g & 0x03];
+        g >>= 2;
 
-        spiPixel.b[bytePos] = mapBits[rgb.b & 0x03];
-        rgb.b >>= 2;
+        spiPixel.b[bytePos] = mapBits[b & 0x03];
+        b >>= 2;
     }
 
     return spiPixel;
 }
 
-static struct rgbPixel_t&
-    makeRgbPixel(struct rgbPixel_t& pixel, uint8_t r, uint8_t g, uint8_t b)
+static rgbPixel_t&
+    makeRgbPixel(rgbPixel_t& pixel, uint8_t r, uint8_t g, uint8_t b)
 {
     pixel.r = r;
     pixel.g = g;
@@ -136,9 +141,9 @@ static struct rgbPixel_t&
 static void rgbGridClear()
 {
     static const uint16_t GRID_AREA = GRID_HEIGHT * GRID_WIDTH;
-    struct rgbPixel_t * pPixel = &rgbGrid[0];
+    rgbPixel_t * pPixel = &rgbGrid[0];
 
-    struct rgbPixel_t black = makeRgbPixel(black, 0, 0, 0);
+    rgbPixel_t black = makeRgbPixel(black, 0, 0, 0);
 
     for (int i = 0; i < GRID_AREA; i++)
     {
@@ -151,16 +156,114 @@ static void rgbGridPattern(int pattern)
     switch(pattern)
     {
         case 0:
-            // Fall through to default case:
-        default:
             for (int x = 0; x < GRID_WIDTH; x++)
             {
+                // Make a increasingly brighter red on top row:
+                int row = 0;
+                int col = x;
+                rgbPixel_t color = makeRgbPixel(color, 4*x + 1, 0, 0);
+                rgbGrid[row * GRID_WIDTH + col] = color;
+            }
+            break;
+            
+        case 1:
+            for (int x = 0; x < GRID_WIDTH; x++)
+            {
+                // Make a increasingly brighter green on 2nd row:
+                int row = 1;
+                int col = x;
+                rgbPixel_t color = makeRgbPixel(color, 0, 4*x + 1, 0);
+                rgbGrid[row * GRID_WIDTH + col] = color;
+            }
+            break;
+            
+        case 2:
+            for (int x = 0; x < GRID_WIDTH; x++)
+            {
+                // Make a increasingly brighter green on 3rd row:
+                int row = 2;
+                int col = x;
+                rgbPixel_t color = makeRgbPixel(color, 0, 4*x + 1, 0);
+                rgbGrid[row * GRID_WIDTH + col] = color;
+            }
+            break;
+            
+        case 3:
+            for (int x = 0; x < GRID_WIDTH; x++)
+            {
+                // Make a increasingly brighter green on 4th row:
+                int row = 3;
+                int col = x;
+                rgbPixel_t color = makeRgbPixel(color, 0, 4*x + 1, 0);
+                rgbGrid[row * GRID_WIDTH + col] = color;
+            }
+            break;
+            
+        case 4:
+            for (int x = 0; x < GRID_WIDTH; x++)
+            {
+                // Make a increasingly brighter green on 5th row:
+                int row = 4;
+                int col = x;
+                rgbPixel_t color = makeRgbPixel(color, 0, 4*x + 1, 0);
+                rgbGrid[row * GRID_WIDTH + col] = color;
+            }
+            break;
+            
+        case 5:
+            for (int x = 0; x < GRID_WIDTH; x++)
+            {
+                // Make a increasingly brighter green on 6th row:
+                int row = 5;
+                int col = x;
+                rgbPixel_t color = makeRgbPixel(color, 0, 4*x + 1, 0);
+                rgbGrid[row * GRID_WIDTH + col] = color;
+            }
+            break;
+            
+        case 6:
+            for (int x = 0; x < GRID_WIDTH; x++)
+            {
+                // Make a increasingly brighter green on 7th row:
+                int row = 6;
+                int col = x;
+                rgbPixel_t color = makeRgbPixel(color, 0, 4*x + 1, 0);
+                rgbGrid[row * GRID_WIDTH + col] = color;
+            }
+            break;
+            
+        case 7:
+            for (int x = 0; x < GRID_WIDTH; x++)
+            {
+                // Make a increasingly brighter green on 8th row:
+                int row = 7;
+                int col = x;
+                rgbPixel_t color = makeRgbPixel(color, 0, 4*x + 1, 0);
+                rgbGrid[row * GRID_WIDTH + col] = color;
+            }
+            break;
+            
+        case 15:
+            for (int x = 0; x < GRID_WIDTH; x++)
+            {
+                // Make a increasingly brighter green on bottom row:
+                int row = GRID_HEIGHT-1;
+                int col = x;
+                rgbPixel_t color = makeRgbPixel(color, 0, 4*x + 1, 0);
+                rgbGrid[row * GRID_WIDTH + col] = color;
+            }
+            break;
+
+        case -1:
+        default:
+            printf("UNKNOWN pattern: %d.  Using default: Blue diag.\n");
+            for (int x = 0; x < GRID_WIDTH; x++)
+            {
+                // Make a increasingly brighter BLUE 
+                // on diagonal from upper left to lower right:
                 int row = x;
                 int col = x;
-                struct rgbPixel_t color;
-
-                // Make a increasingly brighter BLUE:
-                color = makeRgbPixel(color, 0, 0, 2*x);
+                rgbPixel_t color = makeRgbPixel(color, 0, 0, 4*x + 1);
                 rgbGrid[row * GRID_WIDTH + col] = color;
             }
             break;
@@ -170,7 +273,6 @@ static void rgbGridPattern(int pattern)
 static void copySpiGridBytes()
 {
     static const uint16_t GRID_END = sizeof(spiGrid);
-    static const uint16_t TBUF_END = sizeof(txBuffer);
     uint8_t * pBuf = &txBuffer[0];
     uint8_t * pSpiGrid = (uint8_t*)(&spiGrid[0]);
 
@@ -186,21 +288,88 @@ static void gridConvertBits()
     const int MAX_COL = GRID_WIDTH - 1;
     for (int row = 0; row < GRID_HEIGHT; row++)
     {
+//        printf("Row: %d\n", row);
         for (int col = 0; col < GRID_WIDTH; col++)
         {
-            struct spiRgbPixel_t spiPixel
-                = makeSpiPixel(spiPixel, rgbGrid[row * GRID_WIDTH + col]);
+//            printf("Making spiPixel # %d\n", row*GRID_WIDTH+col);
+            spiRgbPixel_t spiPixel;
+                spiPixel = makeSpiPixel(spiPixel, rgbGrid[row * GRID_WIDTH + col]);
 
             if (row & 1)
             {
-                spiGrid[row * GRID_WIDTH + MAX_COL - col] = spiPixel;
+                spiGrid[row * GRID_WIDTH + col] = spiPixel;
             }
             else
             {
-                spiGrid[row * GRID_WIDTH + col] = spiPixel;
+                spiGrid[row * GRID_WIDTH - col + MAX_COL] = spiPixel;
             }
         }
     }
+}
+
+static void dumpSpiGrid()
+{
+    printf("Dumping SPI RGB Grid values:\n");
+    
+    const int MAX_COL = GRID_WIDTH - 1;
+    for (int row = 0; row < GRID_HEIGHT; row++)
+    {
+        printf("Row: %d\n", row);
+        for (int col = 0; col < GRID_WIDTH; col++)
+        {
+            spiRgbPixel_t pixel;
+            
+            if (row & 1)
+            {
+                pixel = spiGrid[row * GRID_WIDTH + col];
+            }
+            else
+            {
+                pixel = spiGrid[row * GRID_WIDTH - col + MAX_COL];
+            }
+            
+            printf("%02X:%02X:%02X:%02X ", pixel.r[0], pixel.r[1], pixel.r[2], pixel.r[3] );
+            printf("%02X:%02X:%02X:%02X ", pixel.g[0], pixel.g[1], pixel.g[2], pixel.g[3] );
+            printf("%02X:%02X:%02X:%02X ", pixel.b[0], pixel.b[1], pixel.b[2], pixel.b[3] );
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+static void dumpRgbGrid()
+{
+    printf("Dumping RGB Grid values:\n");
+    for (int row = 0; row < GRID_HEIGHT; row++)
+    {
+        printf("Row: %d\n", row);
+        for (int col = 0; col < GRID_WIDTH; col++)
+        {
+            rgbPixel_t pixel = rgbGrid[row * GRID_WIDTH + col];
+            printf("%02X:%02X:%02X ", pixel.r, pixel.g, pixel.b);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+static void dumpTxBuffer()
+{
+    printf("Dumping SPI TX Buffer values:\n");
+    uint8_t* pByte = &txBuffer[0];
+    
+    for (int row = 0; row < GRID_HEIGHT; row++)
+    {
+        printf("Row: %d\n", row);
+        // (4 bytes per color) * (3 colors per pixel)
+        for (int col = 0; col < GRID_WIDTH*SPI_BYTES_PER_BYTE*3; col += 4)
+        {
+            printf("%02X:%02X:%02X:%02X ", pByte[0], pByte[1], pByte[2], pByte[3]);
+            pByte += 4;
+        }
+        printf("\n");
+    }
+    printf("\n");
 }
 
 static void gridTransfer(int fd)
@@ -245,6 +414,7 @@ static void print_usage(const char *prog)
     puts("  -D --device   device to use (default /dev/spidev0.0)\n"
          "  -s --speed    max speed (Hz)\n"
          "  -d --delay    delay (use)\n"
+         "  -p --pattern  pattern# to display\n"
          "  -f --file     file (BMP image to load)\n"
     );
     exit(1);
@@ -257,12 +427,13 @@ static void parse_opts(int argc, char *argv[])
             { "device",  1, 0, 'D' },
             { "speed",   1, 0, 's' },
             { "delay",   1, 0, 'd' },
+            { "pattern", 1, 0, 'p' },
             { "file",    1, 0, 'f' },
             { NULL, 0, 0, 0 },
         };
         int c;
 
-        c = getopt_long(argc, argv, "D:s:d:f", lopts, NULL);
+        c = getopt_long(argc, argv, "D:s:d:p:f", lopts, NULL);
 
         if (c == -1)
             break;
@@ -276,6 +447,9 @@ static void parse_opts(int argc, char *argv[])
             break;
         case 'd':
             delay = atoi(optarg);
+            break;
+        case 'p':
+            pattern = atoi(optarg);
             break;
         case 'f':
             file = optarg;
@@ -335,14 +509,15 @@ int main(int argc, char *argv[])
     printf("bits per word: %d\n", bits);
     printf("max speed: %d Hz (%d KHz)\n", speed, speed/1000);
 
-    // 1) Clear the RGB grid
+    // 1) Clear the RGB grid once.
     rgbGridClear();
+    spiGridClear();
 
     // 2) Plot a pattern to the RGB grid
     if (file == NULL)
     {
-        printf("No image file selected. Using default pattern.\n");
-        //rgbGridPattern(0);
+        printf("No image file selected. Using pattern: %d\n", pattern);
+        rgbGridPattern(pattern);
     }
     else
     {
@@ -351,7 +526,12 @@ int main(int argc, char *argv[])
 
     // 3) Transfer the grid data out to the real RGB LED Grid.
     gridTransfer(fd);
-
+    
+    // 4) Get some DEBUG OUT
+    dumpRgbGrid();
+    dumpSpiGrid();
+    dumpTxBuffer();
+    
     close(fd);
 
     return ret;
